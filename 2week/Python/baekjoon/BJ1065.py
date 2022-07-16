1fbf72794fe78f5b353165e56f4afd45920997e2
kangdh208 <kangdh208@gmail.com>
2022-07-17 08:34:53 +0900

    bj modified

--- 2week/Python/baekjoon/BJ1065.py
+++ 2week/Python/baekjoon/BJ1065.py
@@ -1,2 +1,14 @@
 # 어떤 양의 정수 X의 각 자리가 등차수열을 이룬다면, 그 수를 한수라고 한다. 등차수열은 연속된 두 개의 수의 차이가 일정한 수열을 말한다. N이 주어졌을 때, 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력하는 프로그램을 작성하시오. 
 
+def hansu(num) :
+    hansu_cnt = 0
+    for i in range(1, num+1):
+        num_list = list(map(int,str(i)))
+        if i < 100:
+            hansu_cnt += 1  # 100보다 작으면 모두 한수
+        elif num_list[0]-num_list[1] == num_list[1]-num_list[2]:
+            hansu_cnt += 1  # x의 각 자리가 등차수열이면 한수
+    return hansu_cnt
+
+num = int(input())
+print(hansu(num))
