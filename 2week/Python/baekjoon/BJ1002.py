290cd9b1ad0366ae89cf339c08fb23fee8f50453
kangdh208 <kangdh208@gmail.com>
2022-08-18 21:10:24 +0900

    BJ

--- 2week/Python/baekjoon/BJ1002.py
+++ 2week/Python/baekjoon/BJ1002.py
@@ -9,11 +9,27 @@ input = sys.stdin.readline
 T = int(input())
 for _ in range(T):
     x1,y1,r1,x2,y2,r2 = map(int, input().split())
-    d = (((x2-x1)**2) + ((y2-y1)**2))**(0.5)
-    r = r1+r2
-    if d > r:
+    d = (((x2-x1)**2) + ((y2-y1)**2))*(0.5)
+    if d > r1 + r2:
         print(2)
-    elif d == r:
+    elif d == r1 + r2:
         print(1)
     else:
         print(0)
+
+# 긁풀
+import math
+
+n = int(input())
+
+for _ in range(n):
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    distance = math.sqrt((x1-x2)**2 + (y1-y2)**2)  # 두 원의 거리 (원의방정식활용)
+    if distance == 0 and r1 == r2 :  # 두 원이 동심원이고 반지름이 같을 때
+        print(-1)
+    elif abs(r1-r2) == distance or r1 + r2 == distance:  # 내접, 외접일 때
+        print(1)
+    elif abs(r1-r2) < distance < (r1+r2) :  # 두 원이 서로다른 두 점에서 만날 때
+        print(2)
+    else:
+        print(0)  # 그 외에
