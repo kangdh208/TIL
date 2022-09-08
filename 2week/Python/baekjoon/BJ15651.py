51b5242c0e597e36e68f8992ddf61423eb3accea
kangdh208 <kangdh208@gmail.com>
2022-09-08 09:56:48 +0900

    BJ

---
+++ 2week/Python/baekjoon/BJ15651.py
@@ -0,0 +1,18 @@
+# 자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+
+# 1부터 N까지 자연수 중에서 M개를 고른 수열
+# 같은 수를 여러 번 골라도 된다.
+
+import sys
+input = sys.stdin.readline
+
+N,M = map(int, input().split())
+lst = []
+def dfs():
+    if len(lst) == M:
+        print(' '.join(map(str,lst)))
+    for i in range(1,N+1):
+        lst.append(i)
+        dfs()
+        lst.pop()
+dfs()
