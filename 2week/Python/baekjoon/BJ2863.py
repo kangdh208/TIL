5b4e114acc2651b4fb5a26a4ede778fdc60b36a6
kangdh208 <kangdh208@gmail.com>
2022-09-13 10:52:32 +0900

    BJ

--- 2week/Python/baekjoon/BJ2863.py
+++ 2week/Python/baekjoon/BJ2863.py
@@ -22,6 +22,6 @@
 
 A,B = map(int,input().split())
 C,D = map(int,input().split())
-number = [(A/C)+(B/D),(C/D)+(A/B),(D/B)+(C/A),(B/A)+(C/A)]
+number = [(A/C)+(B/D),(C/D)+(A/B),(D/B)+(C/A),(B/A)+(D/C)]
 maxnum = max(number)
 print(number.index(maxnum))
