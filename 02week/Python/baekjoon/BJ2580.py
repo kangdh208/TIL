650ac2c5f2100fcf8019d5c410f21547faa4f362
kangdh208 <kangdh208@gmail.com>
2022-09-30 12:25:12 +0900

    BJ

--- 02week/Python/baekjoon/BJ2580.py
+++ 02week/Python/baekjoon/BJ2580.py
@@ -18,56 +18,36 @@
 
 import sys
 
-graph = []
-blank = []
 
-for i in range(9):
-    graph.append(list(map(int, sys.stdin.readline().rstrip().split())))
-
-for i in range(9):
-    for j in range(9):
-        if graph[i][j] == 0:
-            blank.append((i, j))
+board = [list(map(int, input().split())) for _ in range(9)]
+zeros = [(i, j) for i in range(9) for j in range(9) if board[i][j] == 0]
 
 
-def checkRow(x, a):
+def get_possibles(r, c):
+    possibles = set(range(1, 10))
+    possibles -= set(board[r])
+    test = set()
     for i in range(9):
-        if a == graph[x][i]:
-            return False
-    return True
+        test.add(board[i][c])
+    possibles -= test
+    test = set()
+    for i in range(r//3*3, r//3*3+3):
+        for j in range(c//3*3, c//3*3+3):
+            test.add(board[i][j])
+    possibles -= test
+    return tuple(possibles)
 
 
-def checkCol(y, a):
-    for i in range(9):
-        if a == graph[i][y]:
-            return False
-    return True
+def solve(i):
+    if i == len(zeros):
+        [print(*row) for row in board]
+        sys.exit()
+    r, c = zeros[i]
+    possibles = get_possibles(r, c)
+    for num in possibles:
+        board[r][c] = num
+        solve(i+1)
+        board[r][c] = 0
 
 
-def checkRect(x, y, a):
-    nx = x // 3 * 3
-    ny = y // 3 * 3
-    for i in range(3):
-        for j in range(3):
-            if a == graph[nx + i][ny + j]:
-                return False
-    return True
-
-
-def dfs(idx):
-    if idx == len(blank):
-        for i in range(9):
-            print(*graph[i])
-        exit(0)
-
-    for i in range(1, 10):
-        x = blank[idx][0]
-        y = blank[idx][1]
-
-        if checkRow(x, i) and checkCol(y, i) and checkRect(x, y, i):
-            graph[x][y] = i
-            dfs(idx + 1)
-            graph[x][y] = 0
-
-
-dfs(0)
+solve(0)/
