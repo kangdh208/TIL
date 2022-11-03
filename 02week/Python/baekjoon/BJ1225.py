9615423c513e6adda996bd6b51395ee4e3780ae6
kangdh208 <kangdh208@gmail.com>
2022-11-04 08:35:51 +0900

    BJ

---
+++ 02week/Python/baekjoon/BJ1225.py
@@ -0,0 +1,22 @@
+# A×B를 계산하다 지겨워진 형택이는 A×B를 새로운 방법으로 정의하려고 한다.
+
+# A에서 한 자리를 뽑고 × B에서 임의로 한 자리를 뽑아 곱한다.
+
+# 의 가능한 모든 조합 (A가 n자리, B가 m자리 수라면 총 가능한 조합은 n×m개)을 더한 수로 정의하려고 한다.
+
+# 예를 들어 121×34는
+
+# 1×3 + 1×4 + 2×3 + 2×4 + 1×3 + 1×4 = 28
+
+# 이 된다. 이러한 형택이의 곱셈 결과를 구하는 프로그램을 작성하시오.
+import sys
+
+input = sys.stdin.readline
+A, B = input().split()
+AList = list(A)
+BList = list(B)
+answer = 0
+for i in range(len(AList)):
+    for j in range(len(BList)):
+        answer += int(AList[i]) * int(BList[j])
+print(answer)
