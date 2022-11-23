02dabbd4beacbd3b8d6d694aaf3e8741885c057a
kangdh208 <kangdh208@gmail.com>
2022-11-24 08:46:41 +0900

    BJ

--- 02week/Python/baekjoon/BJ1205.py
+++ 02week/Python/baekjoon/BJ1205.py
@@ -16,10 +16,10 @@ N, T, P = map(int, input().split())
 if N == 0:
     print(-1)
 else:
+    rank = list(map(int, input().split()))
     if N == P and rank[-1] >= T:
         print(-1)
     else:
-        rank = list(map(int, input().split()))
         ranking = N + 1
         for i in range(N):
             if rank[i] <= T:
